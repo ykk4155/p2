5fef3fe299b091b350b400588e1040d1e0af853b
Yashakk <yashashree.kulkarni05@gmail.com>
2021-04-10 17:34:12 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const auth = require('http-auth');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
@@ -40,12 +41,20 @@ router.post('/',
         check('email')
         .isLength({ min: 1 })
         .withMessage('Please enter an email'),
+        check('username')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a username'),
+        check('password')
+        .isLength({ min: 4 })
+        .withMessage('Please enter a password of length atleast 4'),
     ],
-    (req, res) => {
+    async (req, res) => {
         //console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
           const registration = new Registration(req.body);
+          const salt = await bcrypt.genSalt(10);
+          registration.password = await bcrypt.hash(registration.password, salt);
           registration.save()
             .then(() => {
               res.render('thankyou', {
